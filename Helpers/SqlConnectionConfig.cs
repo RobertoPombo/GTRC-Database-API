3940e9acb733d581a3988341212ec689bd06ee76
RobertoPombo <captn.sponge2@web.de>
2024-01-15 21:05:38 +0100

    Improved SqlConnectionConfig (requires adjustment of property names in json file).

--- Helpers/SqlConnectionConfig.cs
+++ Helpers/SqlConnectionConfig.cs
@@ -1,21 +1,69 @@
-﻿using System.Text.Json.Serialization;
+﻿using Dapper;
+using Newtonsoft.Json;
+using Microsoft.Data.SqlClient;
+
+using GTRC_Basics;
 
 namespace GTRC_Database_API.Helpers
 {
-    public class SqlConnectionConfig
+    public class SqlConnectionConfig : ConnectionConfig
     {
         private static readonly string defConStr = "Data Source=@\\@,@;Initial Catalog=@;User ID=@;Password=@;Integrated Security=True@;TrustServerCertificate=true";
 
-        public SqlConnectionConfig() { }
+        public static readonly List<SqlConnectionConfig> List = [];
 
-        public bool IsLocal { get; set; } = false;
-        public string SourceName { get; set; } = string.Empty;
-        public string CatalogName { get; set; } = string.Empty;
-        public string PCName { get; set; } = string.Empty;
-        public string IP6Address { get; set; } = string.Empty;
-        public int Port { get; set; } = 1433;
-        public string UserID { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public SqlConnectionConfig() { List.Add(this); Name = name; }
+
+        private string name = "Preset #1";
+        private bool isActive = false;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                int nr = 1;
+                string delimiter = " #";
+                string defName = name;
+                string[] defNameList = defName.Split(delimiter);
+                if (defNameList.Length > 1 && Int32.TryParse(defNameList[^1], out _)) { defName = defName[..^(defNameList[^1].Length + delimiter.Length)]; }
+                while (!IsUniqueName())
+                {
+                    name = defName + delimiter + nr.ToString();
+                    nr++; if (nr == int.MaxValue) { break; }
+                }
+            }
+        }
+
+        public bool IsActive
+        {
+            get { return isActive; }
+            set { if (value != isActive) { if (value) { DeactivateAllConnections(); } isActive = value; } }
+        }
+
+        public bool IsUniqueName()
+        {
+            int listIndexThis = List.IndexOf(this);
+            for (int conNr = 0; conNr < List.Count; conNr++)
+            {
+                if (List[conNr].Name == name && conNr != listIndexThis) { return false; }
+            }
+            return true;
+        }
+
+        public static SqlConnectionConfig? GetActiveConnection()
+        {
+            foreach (SqlConnectionConfig con in List) { if (con.IsActive) { return con; } }
+            return null;
+        }
+
+        public static void DeactivateAllConnections()
+        {
+            SqlConnectionConfig? con = GetActiveConnection();
+            if (con is not null) { con.IsActive = false; }
+        }
+
         [JsonIgnore] public string ConnectionString
         {
             get
@@ -24,23 +72,58 @@ namespace GTRC_Database_API.Helpers
                 _defConStr[1] += SourceName;
                 _defConStr[2] += Port.ToString();
                 _defConStr[3] += CatalogName;
-                _defConStr[4] += UserID;
+                _defConStr[4] += UserId;
                 _defConStr[5] += Password;
-                if (IsLocal)
+                if (NetworkType == NetworkType.Localhost)
                 {
-                    _defConStr[0] += PCName;
+                    _defConStr[0] += PcName;
                     _defConStr[2] = string.Empty;
                     _defConStr[4] = string.Empty;
                     _defConStr[5] = string.Empty;
                 }
                 else
                 {
-                    _defConStr[0] += IP6Address;
+                    _defConStr[0] += Ipv6;
                     _defConStr[6] = string.Empty;
                 }
                 return string.Join(string.Empty, _defConStr);
             }
             set { }
+        }
+
+        public bool Connectivity()
+        {
+            foreach (string tableName in GlobalValues.SqlTableNames.Values)
+            {
+                string SqlQry = "SELECT TOP 0 * FROM " + tableName ?? string.Empty + ";";
+                try
+                {
+                    SqlConnection con = new(ConnectionString);
+                    _ = con.Query<dynamic>(SqlQry).ToList();
+                }
+                catch { return false; }
+            }
+            return true;
+        }
+
+        public bool Reseed(Type modelType)
+        {
+            if (Connectivity() && GlobalValues.SqlTableNames.TryGetValue(modelType, out string? _tableName))
+            {
+                string tableName = _tableName ?? string.Empty;
+                string SqlQry = "DELETE FROM " + tableName + "; DBCC CHECKIDENT(" + tableName + ", RESEED, 0);";
+                try
+                {
+                    SqlConnection con = new(ConnectionString);
+                    con.Open();
+                    SqlCommand SqlCmd = new(SqlQry, con);
+                    SqlCmd.ExecuteNonQuery();
+                    con.Close();
+                    return true;
+                }
+                catch { return false; }
+            }
+            else { return false; }
         }
     }
 }
